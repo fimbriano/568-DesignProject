dbd9044c7abdf971d09654d6d652b969c2ef4835
Felicia Imbriano <fimbriano@umassd.edu>
2019-04-13 23:42:27 -0400

    added legend

--- designs.js
+++ designs.js
@@ -7,32 +7,35 @@ function lollipop(gameData) {
     //Create Tooltip
     var tooltip = d3.select("#chart")
         .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+            .attr("class", "tooltip")
+            .style("opacity", 0);
 
     //Create SVG
     var svg = d3.select("#chart")
         .append("svg")
+            .attr("class", "chartSVG")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
+            .attr("class", "graph")
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
     
     //Color Scale
-    var labels = ["PS", "XBOX", "PC", "Nintento", "Other"]
+    var labels = ["PS", "XBOX", "PC", "Nintento", "Other"];
     var colorScale = d3.scaleOrdinal()
         .domain(labels)
-        .range(d3.schemeAccent)
+        .range(d3.schemeAccent);
 
     //X Axis
     var xAxis = d3.scaleLinear()
         .domain([0,10])
         .range([0, width]);
-    svg.append("g")
+    var x = svg.append("g")
+        .attr("class", "x-axis")
         .attr("transform", "translate(0,"+ height +")")
         .call(d3.axisBottom(xAxis));
-    svg.append("text")
+    x.append("text")
         .attr("transform","translate(" + (width/2) + " ," + (height + margin.top + 25) + ")")
         .style("text-anchor", "middle")
         .text("User Score");
@@ -41,69 +44,116 @@ function lollipop(gameData) {
     var yAxis = d3.scaleLinear()
         .domain([0,100])
         .range([height, 0]);
-    svg.append("g")
+    var y = svg.append("g")
+        .attr("class", "y-axis")
         .call(d3.axisLeft(yAxis));
-    svg.append("text")
+    y.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left -5)
         .attr("x",0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Critic SCore");
+        .text("Critic Score");
 
     //Create drop lines
-    var rect = svg.selectAll("dropline")
+    var droplines = svg.append("g")
+        .attr("class", "droplines");
+    var rect = droplines.selectAll("dropline")
         .data(gameData)
         .enter()
         .append("rect")
-        .attr("x", d => xAxis(d.User_Score))
-        .attr("y", d => yAxis(d.Critic_Score))
-        .attr("width", .5)
-        .attr("height", d => height - yAxis(d.Critic_Score))
-        .attr("fill", function(d) {
-            console.log(colorScale(d.Platform_Group));
-            return colorScale(d.Platform_Group);
-        })
-        .attr("stroke", "black")
-        .attr("class", "dropline");
+            .attr("x", d => xAxis(d.User_Score))
+            .attr("y", d => yAxis(d.Critic_Score))
+            .attr("width", .5)
+            .attr("height", d => height - yAxis(d.Critic_Score))
+            .attr("fill", function(d) {
+                return colorScale(d.Platform_Group);
+            })
+            .attr("stroke", "black")
+            .attr("class", "dropline");
 
     //Create pacman
-    var pacman = svg.selectAll("pacman")
+    var pacmanMark = svg.append("g")
+        .attr("class", "pacman");
+    var pacman = pacmanMark.selectAll("pacman")
         .data(gameData)
         .enter()
         .append("circle")
-        .attr("cx", function(d) { return xAxis(d.User_Score); })
-        .attr("cy", function(d) { return yAxis(d.Critic_Score); })
-        .attr("r", "1.5%")
-        .style("fill", "none")
-        .attr("stroke", "gold")
-        .attr("stroke-dasharray", "63, 17")
-        .attr("stroke-dashoffset", -5)
-        .attr("stroke-width", "2.5%")
-        .attr("class", "pacman");
+            .attr("cx", function(d) { return xAxis(d.User_Score); })
+            .attr("cy", function(d) { return yAxis(d.Critic_Score); })
+            .attr("r", "1.5%")
+            .style("fill", "none")
+            .attr("stroke", "gold")
+            .attr("stroke-dasharray", "63, 17")
+            .attr("stroke-dashoffset", -5)
+            .attr("stroke-width", "2.5%")
+            .attr("class", "pacman");
 
     //Create point markers
-    var circles = svg.selectAll("point")
+    var points = svg.append("g")
+        .attr("class", "points");
+    var circles = points.selectAll("point")
         .data(gameData)
         .enter()
         .append("circle")
-        .attr("cx", function(d) { return xAxis(d.User_Score); })
-        .attr("cy", function(d) { return yAxis(d.Critic_Score); })
-        .attr("r", "4")
-        //.style("fill", "#69b3a2")
-        .attr("fill", function(d) {
-            console.log(colorScale(d.Platform_Group));
-            return colorScale(d.Platform_Group);
-        })
-        .attr("stroke", "black")
-        .attr("class", "point")
+            .attr("cx", function(d) { return xAxis(d.User_Score); })
+            .attr("cy", function(d) { return yAxis(d.Critic_Score); })
+            .attr("r", "4")
+            //.style("fill", "#69b3a2")
+            .attr("fill", function(d) {
+                return colorScale(d.Platform_Group);
+            })
+            .attr("stroke", "black")
+            .attr("class", "point")
+            .on("mouseover", function(d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html("<strong>Global Players:</strong> " + d.Global_players + 
+                    "<br/><strong>Critic Score:</strong> " + d.Critic_Score +
+                    "<br/><strong>User SCore:</strong> " + d.User_Score +
+                    "<br/><strong>Platform</strong> " + d.Platform_Group)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+    //Legend
+    var legendDiv = d3.select("#chart")
+        .append("div")
+            .attr("id", "legend")
+            .html("<h3>Legend</h3><br/>");
+    var svg2 = legendDiv.append("svg")
+            .attr("width", 500)
+            .attr("height", 500);
+    var legendSVG = svg2.append("svg")
+            .attr("viewbox", "0 0 100 100")
+            .attr("class", "legend");
+    var legendPacman = legendSVG.append("circle")
+        .attr("cx", "20%")
+        .attr("cy", "10%")
+        .attr("r", "5%")
+        .attr("fill", "none")
+        .attr("stroke", "gold")
+        .attr("stroke-dasharray", "130, 50")
+        .attr("stroke-dashoffset", -12)
+        .attr("stroke-width", "10%");
+    var mainCircle = svg2.append("circle")
+        .attr("cx", 100)
+        .attr("cy", 50)
+        .attr("r", 10)
+        .attr("fill", "black")
         .on("mouseover", function(d) {
             tooltip.transition()
                 .duration(200)
                 .style("opacity", .9);
-            tooltip.html("<strong>Global Players:</strong> " + d.Global_players + 
-                "<br/><strong>Critic Score:</strong> " + d.Critic_Score +
-                "<br/><strong>User SCore:</strong> " + d.User_Score)
+            tooltip.html("<strong>Global Players:</strong> " + 
+                "<br/><strong>Critic Score:</strong> " +
+                "<br/><strong>User Score:</strong> ")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
         })
@@ -112,6 +162,47 @@ function lollipop(gameData) {
                 .duration(500)
                 .style("opacity", 0);
         });
+
+    var line = legendSVG.append("line")
+        .attr("x1", 100)
+        .attr("y1", 50)
+        .attr("x2", 70)
+        .attr("y2", 150)
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
+
+    var circleX = 25;
+    circleY = 25;
+    var scaleSVG = svg2.append("g")
+            .attr("class", "legend-scale");
+    var scale = scaleSVG.append("rect")
+            .attr("x", margin.left-5)
+            .attr("y", 150)
+            .attr("width", 150)
+            .attr("height", 255)
+            .attr("stroke", "black")
+            .attr("fill", "white");
+    var legendGroups = scaleSVG.append("g")
+        .attr("transform", "translate(25, 155)");
+    var legendContents = legendGroups.selectAll("g")
+        .data(labels)
+        .enter()
+        .append("g")
+            .attr("transform", function (d,i) {
+                return "translate("+ circleX +","+ circleY*i*2+")";
+            })
+    var legendMarks = legendContents.append("circle")
+            .attr("cx", circleX)
+            .attr("cy", circleY)
+            .attr("r", 20)
+            .attr("fill", d => {
+                console.log(colorScale(d));
+                return colorScale(d);
+            });
+    var legendLabels = legendContents.append("text")
+            .attr("x", circleX + 30)
+            .attr("y", circleY + 5)
+            .text(d => d);
 }
 
 function chart(gameData) {
@@ -148,7 +239,7 @@ function createVis(data) {
     //Call function
     lollipop(gameData);
 
-    chart(gameData);
+    //chart(gameData);
 }
 
 d3.csv("video_game.csv")
